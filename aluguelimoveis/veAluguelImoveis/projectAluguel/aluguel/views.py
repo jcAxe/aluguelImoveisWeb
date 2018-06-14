93f9d767dcbb3748af6c7caf423f7b5e0b95d3ff
joseh <jcamachado@id.uff.br>
2018-06-14 12:02:54 -0300

    Base do projeto.

---
+++ aluguelimoveis/veAluguelImoveis/projectAluguel/aluguel/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+#from django.http import HttpResponse
+
+from django.shortcuts import render
+
+def index(request):
+    # return HttpResponse("Olá Mundo! Você está vendo o conteúdo da view index da aplicação 'vendas'")
+    frase = "Olá mundo"
+    return render(request, 'aluguel/index.html', {'frase': frase})
+
+def pagina1(request):
+    frase = "Você está na página 1"
+    return render(request, 'aluguel/pagina1.html', {'frase': frase})
